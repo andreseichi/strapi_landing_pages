c31702f11c67b9990e86c47df294851fe8467455
André Tashiro <andre_seichi@hotmail.com>
2021-11-26 15:42:50 -0300

    new repo

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.1jj9s.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi_landing_pages'),
+        username: env('DATABASE_USERNAME', 'usuario_landing_page'),
+        password: env('DATABASE_PASSWORD', 'hwUTzvrzfe8DEGLN'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fe4e9163-895a-486e-81a8-6bdc1bb78101'
+};
